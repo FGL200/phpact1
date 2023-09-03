e9029936188a5cc05d2f224f84e40b47d763f348
FGL200 <2020-100627@rtu.edu.ph>
2023-09-03 09:45:37 +0800

    added fields in add new item

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 
 <?php function includeFooter($data = []) { extract($data) ?>
 <footer class="text-c color-w p-1 font-xs">
-    An activity accomplished by the group CRUD and submitted to Mr. Noel
+    An activity accomplished by the group CRUD and submitted to Mr. Noel Gutierez
 </footer>
 <!-- CUSTOM CSS -->
 <?php if(isset($js)) foreach($js as $j) {?>
---
+++ js/add_item.js
@@ -0,0 +1,59 @@
+
+let today = new Date();
+let day = (today.getDate() < 10) ? "0" + today.getDate() : today.getDate() ;
+let month = (today.getMonth() < 10) ? "0" + today.getMonth() : today.getMonth();    
+let year = today.getFullYear();
+let date_today = `${year}-${month}-${day}`;
+
+document.getElementById("date_added").value = date_today;
+
+document.querySelectorAll("#id-item-list option").forEach(function(e){
+    e.addEventListener("click", function(option){
+        option.disabled = true;
+    })
+})
+
+function addItem(e){
+    const value = String(e.value);
+    const id = value.split("_")[0];
+    const name = value.split("_")[1];
+    
+    for(let i = 0; i < e.children.length; i++){
+        e.children[i].selected = false;
+    }
+
+    e.children[0].selected = true;
+
+    const item = document.createElement('p');
+    item.setAttribute("class", "card item-ingredient p-1 font-xs flex-r align-i-center g-1");
+    item.setAttribute("data-id", id);
+    item.innerHTML = name;
+
+    const close = document.createElement("button");
+    close.setAttribute("class", "btn flex-r font-xs justify-c-center align-c-center");
+    close.setAttribute("style", "padding: 0; width: 20px; height: 20px; border-radius: 50%; border: none; font-size: 10px;")
+    close.innerHTML = '_';
+    close.addEventListener("click", function() {
+        item.remove();
+    });
+
+    const quantity = document.createElement("input");
+    quantity.setAttribute("class", "font-xs");
+    quantity.setAttribute("style", "border: none; outline: none; border-left: 1px solid rgba(0,0,0,0.2); padding-left: .5rem; background: rgba(0,0,0,0.1);");
+    quantity.setAttribute("type", "number");
+    quantity.setAttribute("value", "0");
+    quantity.setAttribute("min", "0");
+    quantity.setAttribute("max", "10");
+
+    item.appendChild(quantity);
+    item.appendChild(close);
+
+    const itemsHolder = document.getElementById("items-holder");
+    let copy = 0;
+    for(let i = 0; i < itemsHolder.children.length; i ++){
+        if(itemsHolder.children[i].getAttribute('data-id') == id) copy++;
+    }
+    if(copy == 0){
+        itemsHolder.appendChild(item);
+    }
+}
