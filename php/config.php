022e0d4569fb7a7541a8ed6895489ca51a7d0e07
CMC OJT <ojt_2022@motortrade.com.ph>
2023-09-12 14:51:18 +0800

    set defaukt db con to local

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 
-$con = new mysqli('172.0.0.22' /* localhost */, 'dev' /** root */ , 'root', 'addbase_lab1', '32775' /** optional */);
-// $con = new mysqli('localhost', 'root', '', 'addbase_lab1');
+// $con = new mysqli('172.0.0.22' /* localhost */, 'dev' /** root */ , 'root', 'addbase_lab1', '32775' /** optional */);
+$con = new mysqli('localhost', 'root', '', 'addbase_lab1');
 
 
 ?>
